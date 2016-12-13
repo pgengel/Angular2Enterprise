a778b8276631f332f3b21855594cd0e290060d03
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2016-12-13 10:34:36 +0200

    The CanCreateDatabase test is passing.

--- tests/EnterpriseAngular2.Data.Tests/FunctionalTests/DatabaseScenarioTests.cs
+++ tests/EnterpriseAngular2.Data.Tests/FunctionalTests/DatabaseScenarioTests.cs
@@ -1,6 +1,27 @@
-﻿namespace EnterpriseAngular2.Data.Tests.FunctionalTests
+﻿using System;
+using EnterpriseAngular2.Data.Contexts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnterpriseAngular2.Data.Tests.FunctionalTests
 {
+    [TestClass]
     public class DatabaseScenarioTests
     {
+        [TestMethod]
+        public void CanCreateDatabase()
+        {
+            using (var db = new DataContext())
+            {
+                db.Database.Create();
+            }
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            using (var db = new DataContext())
+                if (db.Database.Exists())
+                    db.Database.Delete();
+        }
     }
 }
--- tests/EnterpriseAngular2.Data.Tests/Properties/AssemblyInfo.cs
+++ tests/EnterpriseAngular2.Data.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3179e682-862b-439c-ba96-0abfdaf4ed23")]
+[assembly: Guid("1b90d267-74e2-45e9-8640-a8d74f78774f")]
 
 // Version information for an assembly consists of the following four values:
 //
