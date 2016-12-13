7df761517bb3ae647596a3296d3948632808412a
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2016-12-13 11:29:57 +0200

    added the web api and the unity DI container.

---
+++ src/EnterpriseAngular2.WebApi/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EnterpriseAngular2.Data.Contexts;
+using Microsoft.Owin;
+using Microsoft.Practices.Unity;
+using Owin;
+
+[assembly: OwinStartup(typeof(EnterpriseAngular2.WebApi.Startup))]
+
+namespace EnterpriseAngular2.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            var container = new UnityContainer();
+
+            container.RegisterType<IBusinessContext, BusinessContext>();
+            //container.RegisterType<MainViewModel>();
+
+            //var window = new MainWindow
+            //{
+            //    DataContext = container.Resolve<MainViewModel>()
+            //};
+
+            ConfigureAuth(app);
+        }
+    }
+}
